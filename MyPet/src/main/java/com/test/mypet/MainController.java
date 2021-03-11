8defb5ca0d7f8ce99d14dcb05c71245e1fcd626b
jennyjipark <jennyjipark@gmail.com>
2021-03-11 09:06:59 +0900

    초기

---
+++ MyPet/src/main/java/com/test/mypet/MainController.java
@@ -0,0 +1,14 @@
+package com.test.mypet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value="/template.action")
+	public String template() {
+		return "template";		
+	}
+	
+}
---
+++ MyPet/src/main/webapp/WEB-INF/views/inc/footer.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+
+<footer id="footer">
+	<div class="">여기 하단 부입니다.</div>
+	<div>노푸른 박지현 윤지현 이대홍 이준오 장진영</div>
+	<div class="pfootername">MyHome Corporation.</div>
+</footer>
