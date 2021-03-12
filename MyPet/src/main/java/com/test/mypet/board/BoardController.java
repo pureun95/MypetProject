587cd4e2829ffa7c5973e81dddb3ebc536f3279b
jennyjipark <jennyjipark@gmail.com>
2021-03-12 12:11:01 +0900

    게시판 템플릿

--- MyPet/src/main/java/com/test/mypet/board/BoardController.java
+++ MyPet/src/main/java/com/test/mypet/board/BoardController.java
@@ -1,14 +1,32 @@
 package com.test.mypet.board;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class BoardController {
 	
-	//대홍님 방식으로 해본거
-	@RequestMapping(value="/volunteerList.action")
-	public String template_two() {
-		System.out.println("test");
-		return "board/volunteerList";		
+	
+	//http://localhost:8090/mypet/board/template.action
+	@RequestMapping(value = "/board/template.action", method = { RequestMethod.GET })
+		public String template(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+
+			return "board/template";
+
+	}
+	
+	
+	//http://localhost:8090/mypet/board/volunteerList.action
+	@RequestMapping(value = "/board/volunteerList.action", method = { RequestMethod.GET })
+	public String volunteer_list(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+
+		return "board/volunteerList";
+
 	}
 
 }
