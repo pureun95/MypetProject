63c31ac341ff813ca55b476691790c22ff545241
Jang-JinYoung <jinyoung_8965@naver.com>
2021-03-12 10:37:41 +0900

    수정
    수정

--- MyPet/src/main/java/com/test/mypet/goods/GoodsController.java
+++ MyPet/src/main/java/com/test/mypet/goods/GoodsController.java
@@ -1,5 +1,22 @@
 package com.test.mypet.goods;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class GoodsController {
 
+//	
+//	@RequestMapping(value="/template/template.action")
+//	public String template() {
+//		return "template.template";		
+//	}
+	
+	@RequestMapping(value="/goods/list.action")
+	public String list() {
+		
+		return "goods.list";
+		
+	}
+	
 }
--- MyPet/src/main/webapp/WEB-INF/views/inc/content.jsp
+++ MyPet/src/main/webapp/WEB-INF/views/inc/content.jsp
@@ -1,4 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
+%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <div id="content">
 	<div id="contentnav">개인 서브네비입니다.</div>
