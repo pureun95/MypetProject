8dfe1a97414d8c02394eb2f283a4413d90725235
pureun95 <77381499+pureun95@users.noreply.github.com>
2021-03-16 17:37:14 +0900

    노푸른 풀리퀘

--- MyPet/src/main/java/com/test/mypet/member/MemberService.java
+++ MyPet/src/main/java/com/test/mypet/member/MemberService.java
@@ -7,9 +7,14 @@ public class MemberService {
 	@Autowired
 	IMemberDAO imemberDAO;
 
-	// 로그인
+	// 로그인(유저)
 	public MemberDTO memberLogin(MemberDTO memberDTO) throws Exception {
 		return imemberDAO.memberLogin(memberDTO);
+	}
+	
+	// 로그인(관리자)
+	public AdminDTO adminLogin(AdminDTO adminDTO) throws Exception {
+		return imemberDAO.adminLogin(adminDTO);
 	}
 	
 }
