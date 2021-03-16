d681b02a2dd7027227c9b6c67ae1197df22d2294
jihyun-yoon <wlgus0702@gmail.com>
2021-03-16 18:34:31 +0900

    vet,shelter 수정
    검색추가

--- Mypet/src/main/java/com/test/mypet/vet/IVet.java
+++ Mypet/src/main/java/com/test/mypet/vet/IVet.java
@@ -1,11 +1,16 @@
 package com.test.mypet.vet;
 
+import java.util.HashMap;
 import java.util.List;
+
+import com.test.mypet.shelter.LocationDTO;
 
 public interface IVet {
 
-	List<VetDTO> list();
+	List<VetDTO> list(HashMap<String, String> map);
 
 	VetDTO get(String seq);
+
+
 
 }
