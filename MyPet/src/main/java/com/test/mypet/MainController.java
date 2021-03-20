bce36106d0b62ad4f6446b02382ba1cee3b0e703
jennyjipark <jennyjipark@gmail.com>
2021-03-20 18:57:44 +0900

    메인

--- MyPet/src/main/java/com/test/mypet/MainController.java
+++ MyPet/src/main/java/com/test/mypet/MainController.java
@@ -1,18 +1,37 @@
 package com.test.mypet;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.test.mypet.board.AdoptionReviewDTO;
+import com.test.mypet.board.IAdoptionReviewDAO;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	IAdoptionReviewDAO ardao;
+	
+	
 	@RequestMapping(value="/layout/layout.action")
 	public String template1() {
 		return "layout.layout";		
 	}
 	
 	@RequestMapping(value="/main.action")
-	public String main() {
+	public String main(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+		
+		List<AdoptionReviewDTO> rlist = ardao.getList();
+		   
+		request.setAttribute("rlist", rlist);		   		   
+		   
 		return "mypet.main";		
 	}
 	
