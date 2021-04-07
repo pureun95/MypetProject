13af6c30606eb4682b9fc0de9906ea53e7767575
PUREUN NOH <77381499+pureun95@users.noreply.github.com>
2021-04-07 18:29:27 +0900

    Update NoticeDAO.java

--- MyPet/src/main/java/com/test/mypet/board/NoticeDAO.java
+++ MyPet/src/main/java/com/test/mypet/board/NoticeDAO.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Repository;
 
 /**
  * 공지사항 관련 db전담 객체
- * @author Blue
+ * @author 
  *
  */
 @Repository
@@ -56,57 +56,17 @@ public class NoticeDAO implements INoticeDAO {
 
 //	public static void getDetail(HttpServletRequest request, String seqNotice, NoticeDAO noticeDAO) {
 	public NoticeDTO getDetail(String seqNotice) {
-		//파라미터로 전달되는 글번호
-				//String seqNotice = request.getParameter("seqNotice");
-				
-				//검색과 관련된 파라미터를 읽어와 본다.
-				//String keyword=request.getParameter("keyword");
-				//String condition=request.getParameter("condition");
-				
-				//CafeDto 객체 생성 (select 할때 필요한 정보를 담기 위해)
+		
 				NoticeDTO ndto =new NoticeDTO();
-				
-//				if(keyword != null) {//검색 키워드가 전달된 경우
-//					if(condition.equals("content")) {//제목+내용 검색
-//						ndto.setContent(keyword);
-//					}else if(condition.equals("title")) {//제목 검색
-//						ndto.setTitle(keyword);
-//					}
-//				}
-					//request 에 검색 조건과 키워드 담기
-					//request.setAttribute("condition", condition);
-					/*
-					 *  검색 키워드에는 한글이 포함될 가능성이 있기 때문에
-					 *  링크에 그대로 출력가능하도록 하기 위해 미리 인코딩을 해서
-					 *  request 에 담아준다.
-					 */
-//					String encodedKeyword=null;
-//					
-//					try {
-//						encodedKeyword=URLEncoder.encode(keyword, "utf-8");
-//					} catch (UnsupportedEncodingException e) {
-//						e.printStackTrace();
-//					}
-					//인코딩된 키워드와 인코딩 안된 키워드를 모두 담는다.
-//					request.setAttribute("encodedKeyword", encodedKeyword);
-//					request.setAttribute("keyword", keyword);
 					
 				//Dto 에 글번호도 담기
 				ndto.setSeqNotice(seqNotice);
 				
-				//조회수 1 증가 시키기
-				//NoticeDAO.addViewCount(seqNotice);
-				//글정보를 얻어와서
-				//NoticeDTO ndto2 = NoticeDAO.get(seqNotice);
 				NoticeDAO noticeDAO = new NoticeDAO();
 				
-				 NoticeDTO ndto2 = noticeDAO.get(seqNotice);
-				   
-				
-				//request 에 글정보를 담고 
-				//request.setAttribute("ndto", ndto2);
+				NoticeDTO ndto2 = noticeDAO.get(seqNotice);
+
 				return template.selectOne("notice.getData", seqNotice);
-				//return template.selectOne("notice.get", seqNotice);
 			
 	}
 	
